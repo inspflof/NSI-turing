ed668226985b70ca97ee8c6fc5927f565c53a345
Legendevry <198399951+Legendevry@users.noreply.github.com>
2026-04-10 08:28:37 +0200

    Refactor demarrer function to use variable for case count and improve entry placement logic

--- Interface.py
+++ Interface.py
@@ -1,16 +1,17 @@
 import tkinter as tk
 
 def demarrer():
+    nb_cases = 50
     racine = tk.Tk()
     racine.title("Turing")
     racine.geometry("2500x2500")
     label = tk.Label(racine, text="Turing")
     label.pack(padx=20, pady=20)
     bande = []
-    for i in range(69):
+    for i in range(nb_cases):
         bande.append(tk.Entry(racine, text = "", width = 1, fg = "#6709AF"))
     longueur = len(bande)
-    for i in bande:
-        i.place(x=None, y=-1250)
+    for i in range(longueur):
+        bande[i].place(x=2500//longueur * i, y=-1250)
     racine.mainloop()
 demarrer()
