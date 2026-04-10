d8a1f697f6cc74407317f4f73c50417498f3183b
Legendevry <198399951+Legendevry@users.noreply.github.com>
2026-04-10 09:04:16 +0200

    Fix button command initialization and update label text in demarrer function

--- Interface.py
+++ Interface.py
@@ -2,19 +2,21 @@ import tkinter as tk
 
 def complement_a_2():
     pass
-def supp():
-    pass
+def supp(bande):
+    for i in bande:
+        i.text = ""
+    
 
 def demarrer():
     nb_cases = 50
     racine = tk.Tk()
     racine.title("Turing")
     racine.geometry("2500x2500")
-    label = tk.Label(racine, text="enttrez vos chiffres svp :")
+    label = tk.Label(racine, text="entrez vos chiffres svp :")
     label.pack(padx=20, pady=20)
     bande = []
     for i in range(nb_cases):
-        bande.append(tk.Entry(racine, text = " ", width = 1, fg = "#6709AF", state="readonly"))
+        bande.append(tk.Entry(racine, text = "", width = 1, fg = "#6709AF", state="readonly"))
     longueur = len(bande)
     entree = tk.Entry(racine, width = 20)
     entree.pack(padx=20, pady=20)
@@ -22,7 +24,7 @@ def demarrer():
         bande[i].pack(side="left", padx=0, pady=0, expand=True)
     bouton_complement = tk.Button(racine, text = "complément à 2", command=complement_a_2())
     bouton_complement.pack(padx=20, pady=20)
-    bouton_supprimer = tk.Button(racine, text = "supprimer", command=supp())
+    bouton_supprimer = tk.Button(racine, text = "supprimer", command=supp(bande))
     bouton_supprimer.pack(padx=20, pady=20)
 
     racine.mainloop()
