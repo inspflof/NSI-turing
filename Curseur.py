e1347915ba953b97af881b568fc4854b4be09310
baleyuk <63366148+baleyuk@users.noreply.github.com>
2026-04-10 08:21:32 +0200

    Update Curseur and Machine classes: modify transition table in Curseur, add renvoi_all method, and adjust index checks in Machine's update method

--- Curseur.py
+++ Curseur.py
@@ -5,14 +5,20 @@ class Curseur():
         self.indice = indice
         self.tables = {"E0":{("E0",1):["E0",0,"->"],
                           ("E0",0):["E0",1,"->"],
-                          ("E0",""):["E1","","<-"]},
+                          ("E0",""):["E2","","<-"]},
                     "E1":{("E1",0):["Fin",1,"-"],
                           ("E1",1):["E1",0,"<-"],
-                          ("E1",""):["Fin","","-"]}}
+                          ("E1",""):["Fin","1","-"]},
+                    "E2":{("E2",0):["Fin",1,"-"],
+                          ("E2",1):["E1",0,"<-"],
+                          ("E2",""):["E2","","<-"]}}
+                
         self.deplacements = {"->": 1,
                                 "<-": -1,
                                 "-":0}
     
+    def renvoi_all(self):
+        print(self.etat,self.lettre,self.indice)
 
     def renvoyer_table(self):
         if self.etat == "Fin":
