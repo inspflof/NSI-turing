d64b2ae182f8f6add8017ec8c0d257d39ec0bcbe
Legendevry <198399951+Legendevry@users.noreply.github.com>
2026-04-10 08:49:19 +0200

    Add input prompt for user to enter numbers before creating Entry widgets

--- Interface.py
+++ Interface.py
@@ -5,12 +5,14 @@ def demarrer():
     racine = tk.Tk()
     racine.title("Turing")
     racine.geometry("2500x2500")
-    label = tk.Label(racine, text="Turing")
+    label = tk.Label(racine, text="enttrez vos chiffres svp :")
     label.pack(padx=20, pady=20)
     bande = []
     for i in range(nb_cases):
         bande.append(tk.Entry(racine, text = " ", width = 1, fg = "#6709AF", state="readonly"))
     longueur = len(bande)
+    entree = tk.Entry(racine, width = 20)
+    entree.pack(padx=20, pady=20)
     for i in range(longueur):
         bande[i].pack(side="left", padx=0, pady=0, expand=True)
     racine.mainloop()
